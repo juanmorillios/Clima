7db8783cdd55f1ab9f75311907b88e99ae6af431
Juan Morillo <juanmorillios@gmail.com>
2017-02-17 21:54:45 +0100

    Installing CocoaPods
    Installing CocoaPods

---
+++ Clima/ClimaVC.swift
@@ -0,0 +1,29 @@
+//
+//  ClimaVC.swift
+//  Clima
+//
+//  Created by Juan Morillo on 17/2/17.
+//  Copyright © 2017 Juan Morillo. All rights reserved.
+//
+
+import UIKit
+
+class ClimaVC: UIViewController {
+    
+    @IBOutlet weak var datelabel: UILabel!
+    
+    @IBOutlet weak var currentTempLabel: UILabel!
+
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var curentWeatherImage: UIImageView!
+    
+    @IBOutlet weak var currenWeatherTypeLabel: UILabel!
+    
+    @IBOutlet weak var myTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+  }
